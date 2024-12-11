fd075f4fefaa51b37622d30380c1875d5ec93910
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-12 01:03:10 +0600

    fetch games with hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "@/services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GameListResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "@/hook/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<GameListResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
-        {error && <Text>{error}</Text>}
-        <ul>
+      {error && <Text>{error}</Text>}
+      <ul>
         {games.map((game) => {
-            return <li key={game.id}>{game.name}</li>;
+          return <li key={game.id}>{game.name}</li>;
         })}
-        </ul>
+      </ul>
     </>
   );
 };
