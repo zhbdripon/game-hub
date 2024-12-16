4fc13f310a195363168f5c4d3624682c94622716
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-16 20:07:52 +0600

    display the genre

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 
+const LARGE_SCREEN_WIDTH = 992
+
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -20,18 +22,18 @@ function App() {
     <Grid
       height="100vh"
       templateRows={{ base: "repeat(10, 1fr)" }}
-      templateColumns={{ base: "repeat(10, 1fr)" }}
+      templateColumns={{ base: "200px repeat(9, 1fr)" }}
       gap={1}
     >
       <GridItem rowSpan={1} colSpan={10}>
         <Navbar />
       </GridItem>
-      <Show when={width >= 992}>
-        <GridItem rowSpan={9} colSpan={2}>
+      <Show when={width >= LARGE_SCREEN_WIDTH}>
+        <GridItem rowSpan={9} colSpan={1} paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
-      <GridItem rowSpan={9} colSpan={width >= 992 ? 8 : 10}>
+      <GridItem rowSpan={9} colSpan={width >= LARGE_SCREEN_WIDTH ? 9 : 10}>
         <GameGrid />
       </GridItem>
     </Grid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,21 @@
 import useGenres from "@/hook/useGenres";
+import { getOptimizeImageUrl } from "@/services/image-url";
+import { HStack, Image, List } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
-  return <ul>
-    {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-  </ul>
+  return (
+    <List.Root variant="plain">
+      {genres.map((genre) => (
+        <List.Item key={genre.id} paddingY={1}>
+          <HStack>
+            <Image src={getOptimizeImageUrl(genre.image_background)} boxSize="32px" borderRadius={8} />
+            {genre.name} 
+          </HStack>
+        </List.Item>
+      ))}
+    </List.Root>
+  );
 };
 
 export default GenreList;
