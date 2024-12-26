3a66e2f9289bc43b86783360c57d820ce6d3cbf0
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2024-12-26 17:04:03 +0600

    implement expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,33 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+  value: string;
+}
+
+const ExpandableText = ({ value }: Props) => {
+  const [expand, setExpand] = useState(false);
+
+  if (!value) return null;
+
+  const TEXT_THRESHOLD = 400;
+  const isLargeText = value.length > TEXT_THRESHOLD;
+
+  return (
+    <Text>
+      {isLargeText && !expand ? value.slice(0, TEXT_THRESHOLD)+"... " : value}{" "}
+      {isLargeText && (
+        <Button
+          size="xs"
+          fontWeight="bold"
+          colorPalette="yellow"
+          onClick={() => setExpand(!expand)}
+        >
+          {expand ? "Show Less" : "Show More"}
+        </Button>
+      )}
+    </Text>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,20 +1,20 @@
+import ExpandableText from "@/components/ExpandableText";
 import useGame from "@/hook/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
+import { useState } from "react";
 import { useParams } from "react-router";
 
 const GameDetailPage = () => {
   const { id: gameId } = useParams();
-
   const { data: game, isLoading, error } = useGame(gameId!);
 
   if (isLoading) return <Spinner />;
-
   if (error || !game) throw Error("Game fetch failed");
 
   return (
     <>
       <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <ExpandableText value={game.description_raw} />
     </>
   );
 };
